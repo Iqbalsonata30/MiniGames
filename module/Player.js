f0e474fda2145027171db8455c071a6f2f9587a7
zikriaulia28 <103765843+zikriaulia28@users.noreply.github.com>
2022-06-12 21:03:04 +0700

    Add files via upload

--- module/Player.js
+++ module/Player.js
@@ -1,32 +1,82 @@
-class Player{
-  constructor(){
-    this._username = "";
-  }
-  generateUsername(){
-    const random =~~[Math.random() * 10000];
-    const username = this.username + random.toString();
-    return username;
+class Player {
+  constructor() {
+    this._token = sessionStorage.getItem("token");
+    this._isWin = false;
+    this._gacha = [];
   }
 
-  set username(_username){
-    return this._username = _username;
-  }
-  get username(){
-    return this._username;
+  set token(_token) {
+    return (this._token = _token);
   }
 
-
-  get Register(){
-    this.generateUsername();
-    sessionStorage.setItem('Username',this.generateUsername());
-    setTimeout(()=>{
-      location.href='#start'
-    },500);
+  get token() {
+    return this._token;
   }
 
-  get logout(){
-    sessionStorage.removeItem('Username');
-    location.reload();
+  set register(_username) {
+    const rand = ~~(Math.random() * _username.length);
+    const session = sessionStorage.setItem("token", `${_username}-${rand}`);
+    return (this.token = session);
   }
 
+  set gacha(option) {
+    let gacha = [];
+    for (let i = 0; i < option.length; i++) {
+      const generating = option[~~(Math.random() * option.length)];
+      gacha.push(generating);
+    }
+    return (this._gacha = gacha);
+  }
+
+  get gacha() {
+    return this._gacha;
+  }
+
+  get start() {
+    const rolling = setInterval(() => {
+      this.gacha = default_option;
+      box1.textContent = this.gacha[0];
+      box2.textContent = this.gacha[1];
+      box3.textContent = this.gacha[2];
+    }, 50);
+
+    setTimeout(() => {
+      clearInterval(rolling);
+      const box = [box1.textContent, box2.textContent, box3.textContent];
+      this.winCheck = box;
+      this.winCheck ? this.reward : null;
+    }, 3500);
+  }
+
+  set winCheck(box) {
+    if (box[0] == box[1] && box[0] == box[2]) {
+      console.log("matching!");
+      return (this._isWin = true);
+    } else {
+      console.log("not match");
+      return (this._isWin = false);
+    }
+  }
+
+  get winCheck() {
+    return this._isWin;
+  }
+
+  get reward() {
+    fetch("https://zoo-animal-api.herokuapp.com/animals/rand")
+      .then((x) => x.json())
+      .then((data) => {
+        const img = new Image(200, 200);
+        img.src = data.image_link;
+        imgReward.appendChild(img);
+        rewardSection.style.display = "block";
+        setTimeout(() => {
+          location.href = "#reward";
+        }, 1000);
+      });
+  }
+
+  logout() {
+    return (this.token = sessionStorage.removeItem("token"));
+  }
 }
